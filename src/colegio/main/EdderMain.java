922c5c1da10af34b5dfd1c7d4834ff85ad587deb
Aladdin <ninjalover249@gmail.com>
2022-06-23 18:01:38 -0300

    El regalito a mis compas 🎁

---
+++ src/colegio/main/EdderMain.java
@@ -0,0 +1,14 @@
+package colegio.main;
+
+import colegio.dao.*;
+import colegio.entidades.*;
+import java.util.*;
+import javax.swing.JOptionPane;
+
+public class EdderMain {
+
+  public static void main(String[] args) {
+    
+  }
+  
+}
